68401d596a1ef7256c212bdf5e790e0610a8f0df
qinyinghua <yinghua.qin@sjsu.edu>
2018-11-30 21:53:14 -0800

    added a login integration

--- FrontEnd/lib/login/controllers/login.js
+++ FrontEnd/lib/login/controllers/login.js
@@ -23,7 +23,8 @@ exports.loginForm = function(req, res) {
 exports.loginSubmit = function(req, res) {
 
 	const http = require('http');
- 
+	const querystring = require('querystring'); 
+	
 	var name = req.query.name || "";
 
 	  var context = {
@@ -31,6 +32,51 @@ exports.loginSubmit = function(req, res) {
 	  , welcomeMessage: greeter.welcomeMessage(name)
 	  ,pageDescr: "Welcome to our Starbucks Club"
 	  };
+	  
+	  
+
+	  var postData = querystring.stringify({
+	      'name' : 'oliver',
+	      'password' : '123'
+	  });
+
+	  var options = {
+	    hostname: '52.8.1.184',
+	    port: 3000,
+	    path: '/login',
+	    method: 'POST',
+	    headers: {
+	         'Content-Type': 'application/x-www-form-urlencoded',
+	         'Content-Length': postData.length
+	       }
+	  };
+
+	  var req = http.request(options, (res) => {
+	    console.log('statusCode:', res.statusCode);
+	    console.log('headers:', res.headers);
+	    let data = '';
+		  // A chunk of data has been recieved.
+		  res.on('data', (chunk) => {
+		    data += chunk;
+		  });
+		  // The whole response has been received. Print out the result.
+		  res.on('end', () => {
+		    console.log(data);
+		  });
+
+//	    res.on('data', (d) => {
+//	      process.stdout.write(d);
+//	    });
+	  });
+
+	  req.on('error', (e) => {
+	    console.error(e);
+	  }); 
+	  
+	  req.write(postData);
+	  req.end();	  
+	  
+	  
 
 	  //var template = __dirname + '/../views/login';
 	  //res.render(template, context);
@@ -40,20 +86,40 @@ exports.loginSubmit = function(req, res) {
 	  // res.status(200).send('Hello World');
 	};
 
+	exports.loginSubmit1 = function(req, res) {
 
-	http.get('http://localhost:3001/goapi?api_key=DEMO_KEY', (resp) => {
-	  let data = '';
+		const http = require('http');
+	 
+		var name = req.query.name || "";
 
-	  // A chunk of data has been recieved.
-	  resp.on('data', (chunk) => {
-	    data += chunk;
-	  });
+		  var context = {
+		    siteTitle: "Home"
+		  , welcomeMessage: greeter.welcomeMessage(name)
+		  ,pageDescr: "Welcome to our Starbucks Club"
+		  };
+		  
+		  
+			http.get('http://localhost:3001/ping', (resp) => {
+				  let data = '';
 
-	  // The whole response has been received. Print out the result.
-	  resp.on('end', () => {
-	    console.log(JSON.parse(data).explanation);
-	  });
+				  // A chunk of data has been recieved.
+				  resp.on('data', (chunk) => {
+				    data += chunk;
+				  });
+				  // The whole response has been received. Print out the result.
+				  resp.on('end', () => {
+				    console.log(data);
+				  });
 
-	}).on("error", (err) => {
-	  console.log("Error: " + err.message);
-	});
+				}).on("error", (err) => {
+				  console.log("Error: " + err.message);
+				});
+
+		  //var template = __dirname + '/../views/login';
+		  //res.render(template, context);
+		  res.redirect('/mycards');
+		  
+		  // Just responding with a string, without any template:
+		  // res.status(200).send('Hello World');
+		};
+
